ee33c6b9f3326db04117ff7ec79c77111f262971
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2020-08-12 12:40:14 -0300

    Creating page header | Study

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
+import './styles.css';
 
 
 function TeacherList() {
@@ -12,9 +13,7 @@ function TeacherList() {
                     <Link to="/">
                         <img src={backIcon} alt="Voltar" />
                     </Link>
-                    <Link to="/">
                         <img src={logoImg} alt="Proffy" />
-                    </Link>
                 </div>
                 <div className="header-content">
                     <strong>Estes são os proffys disponíveis.</strong>
