c5d8f449946b3eda1e7e46f1730a417aa8dca243
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2020-08-12 20:21:42 -0300

    Importing TeacherItem

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 import './styles.css';
 
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
-        <PageHeader title="Estes são os proffys disponíveis.">
+        <PageHeader title="Estes são os proffys disponíveis:">
             <form id="search-teachers">
                 <div className="input-block">
                     <label htmlFor="subject">Matéria</label>
@@ -23,6 +24,14 @@ function TeacherList() {
                 </div>
             </form>
         </PageHeader>
+
+        <main>
+          <TeacherItem />
+          <TeacherItem />
+          <TeacherItem />
+          <TeacherItem />
+          <TeacherItem />
+        </main>
         </div>
     )
 }
