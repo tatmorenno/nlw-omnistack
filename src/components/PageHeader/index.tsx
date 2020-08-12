5e265910998ba689463c1710efc5a0fe6630583d
Tatiana Moreno <tatiana.emilia.morenno@gmail.com>
2020-08-12 20:19:38 -0300

    Importing CSS

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
+import './styles.css';
 
 interface PageHeaderProps {
     title: string;
